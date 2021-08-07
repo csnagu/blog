0d8386c42c8db3b6efc83925a20f60e40e90782d
csnagu <s.chikara3110@gmail.com>
2021-08-07 23:10:15 +0900

    Remove GA-tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,7 +81,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `UA-17765726-16`,
+        trackingId: ``,
       },
     },
     `gatsby-plugin-feed`,
